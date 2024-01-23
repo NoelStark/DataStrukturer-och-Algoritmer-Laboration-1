459442162712c9f55b7973baf43573b9db29374d
Noel Stark <noel.k.stark@gmail.com>
2024-01-23 13:14:13 +0100

    WriteLine test

--- DataStrukturer och Algoritmer Laboration 1/Class1.cs
+++ DataStrukturer och Algoritmer Laboration 1/Class1.cs
@@ -3,5 +3,11 @@
     public class Class1
     {
         int i = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            Console.WriteLine("Test2");
+        }
     }
 }
